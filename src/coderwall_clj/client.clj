eedd27b9a264dd9148b62457c1e56b3054ae4836
Vladislav Bauer <bauer.vlad@gmail.com>
2015-07-02 03:29:58 +0600

    Add docs in the code

--- src/coderwall_clj/client.clj
+++ src/coderwall_clj/client.clj
@@ -3,25 +3,36 @@
   (:require [clj-http.client :as http]))
 
 
-(def ^:dynamic DEF_API_URL "https://coderwall.com/")
-(def ^:dynamic DEF_FORMAT ".json")
-(def ^:dynamic DEF_HTTP_CONFIG
+; Constants
+
+(def ^:dynamic api-url "https://coderwall.com/")
+(def ^:dynamic output-format-extension ".json")
+(def ^:dynamic http-client-config
   {:as :json
    :accept :json
    :insecure? true
    :throw-exceptions true
+   :follow-redirects true
+   :max-redirects 3
    :socket-timeout 10000
-   :conn-timeout 10000})
+   :conn-timeout 10000
+   :debug false})
 
 
-(defn user-url [user-name]
-  (str DEF_API_URL (name user-name) DEF_FORMAT))
+; Functions
+
+(defn ^:dynamic user-url
+  "Build URL to the Coderwall API to fetch information about user."
+  [user-name]
+  (let [user (name user-name)]
+    (str api-url user output-format-extension)))
 
 (defn user-data
+  "Fetch information about Coderwall user via REST API."
   ([user-name]
    (user-data user-name true))
   ([user-name full?]
-   (user-data user-name full? DEF_HTTP_CONFIG))
+   (user-data user-name full? http-client-config))
   ([user-name full? http-config]
    (let [url (user-url user-name)
          query-params {:query-params {:full full?}}
