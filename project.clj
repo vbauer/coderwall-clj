bf45f7dd922dc194238a2b351cd44aecad2285f7
Vladislav Bauer <bauer.vlad@gmail.com>
2015-07-02 16:03:54 +0600

    Prepare to release

--- project.clj
+++ project.clj
@@ -3,10 +3,18 @@
   :url "https://github.com/vbauer/coderwall-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo}
+            :distribution :repo
+            :comments "Same as Clojure"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "1.1.2"]]
 
+  :profiles {:prod {:plugins [[lein-release "1.0.6" :exclusions [org.clojure/clojure]]]
+                    :scm {:name "git"
+                          :url "https://github.com/vbauer/coderwall-clj"}
+                    :lein-release {:deploy-via :clojars
+                                   :scm :git}}}
+
+  :checksum :warn
   :global-vars {*warn-on-reflection* false}
   :min-lein-version "2.0.0")
